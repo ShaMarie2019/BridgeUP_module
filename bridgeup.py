029edea174b8dbf9b8ac5440f36a5b5a877299e8
shamarie2019 <shaniquemrock@gmail.com>
2019-09-24 18:12:26 -0400

    Committing function

--- bridgeup.py
+++ bridgeup.py
@@ -5,4 +5,15 @@
 
 import pandas as pd
 
-df = pd.read_csv("internship_bootcamp_data.csv")
+df = pd.read_csv("internship_bootcamp_data.csv", error_bad_lines=False)
+#This function is going into the column Fav ice cream flavor from the csv and 
+#counting to see how many like that specfic ice cream flavor. 
+
+def flavor_num(stu_fla):
+    flavor = df["Fav ice cream flavor"]
+    fla_re = 0
+    for i in flavor:
+        if i == stu_fla:
+            fla_re += 1
+    return fla_re
+print(flavor_num("coffee"))
